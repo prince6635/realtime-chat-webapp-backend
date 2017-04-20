5cb7dce941292418bcdc148448dbc950cf15fb0c
Zi Wang <ziwang@uber.com>
2017-04-20 10:35:51 -0700

    web socket

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"github.com/gorilla/websocket"
 )
 
 func main() {
@@ -10,6 +11,38 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+var upgrader = websocket.Upgrader {
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true // disable same origion policy for now
+	},
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	/* http server
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	*/
+
+	// WebSocket server
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		msgType, msg, err := socket.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(string(msg))
+
+		if err = socket.WriteMessage(msgType, msg); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
