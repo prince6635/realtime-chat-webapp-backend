e207262ef706db19c14d92136b35f327ebafddb3
Zi Wang <ziwang@uber.com>
2017-04-20 22:20:31 -0700

    golang channel demo

---
+++ demo/golang_channel.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func GolangChannel() {
+	msgChan := make(chan string)
+	go func() {
+		msgChan <- "Hello, golang channel!"
+	}()
+
+	msg := <- msgChan
+	fmt.Println(msg)
+}
--- demo/main.go
+++ demo/main.go
@@ -1,5 +1,6 @@
 package main
 
 func main() {
-	DecodeEncode()
+	// DecodeEncode()
+	GolangChannel()
 }
